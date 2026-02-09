55cd60657ec1aa50c0c0856d9fd891c52125e16b
65010143 <65010143@kmitl.ac.th>
2026-02-09 21:56:38 +0700

    scan_it.py

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,20 @@
+x = input()
+y = x.split("'")
+y = x.split('"')
+y = list(filter(lambda i:i !=" " and i!="",y))
+n = 0
+if len(y) != 2:
+    print("none")
+else:
+    key = y[0]
+    scan = y[1]
+    scan_list = list(scan.split(" "))
+    for i in scan_list:
+        if i == key:
+            n+=1
+        else:
+            n+=0
+    if n == 0:
+        print("none")
+    else:
+        print(n)
