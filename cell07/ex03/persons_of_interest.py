1c52c06fc96101c544ca4931e5b86a9f0db3706c
65010143 <65010143@kmitl.ac.th>
2026-02-10 11:27:58 +0700

    persons_of_interest.py

--- cell07/ex03/persons_of_interest.py
+++ cell07/ex03/persons_of_interest.py
@@ -4,10 +4,15 @@ women_scientists = {
 "lise": { "name": "Lise Meitner", "date_of_birth": "1878" },
 "grace": { "name": "Grace Hopper", "date_of_birth": "1906" }
 }
+
 def  famous_births(x):
+    result ={}
     for k,v in x.items():
         name_l = v['name']
         birth_l = v['date_of_birth']
-        print(f'{name_l} is a great scientist born in {birth_l}.')
+        result[name_l] = birth_l
+        result_sorted = dict(sorted(result.items(), key=lambda item: item[1]))
+    for k,v in result_sorted.items():
+        print(f'{k} is a great scientist born in {v}.') 
 
 famous_births(women_scientists)
