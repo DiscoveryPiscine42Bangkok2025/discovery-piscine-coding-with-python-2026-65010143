ed740b27a2c1b1ff4120b26121a536b08f5f3dc3
65010143 <65010143@kmitl.ac.th>
2026-02-12 00:57:59 +0700

    Update calculator.py

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,6 +1,11 @@
-x1 = input(f"Give me the first number: ")
-x2 = input(f"Give me the second number: ")
-x1,x2 = int(x1),int(x2)
+import sys
+x1 = sys.argv[1]
+x2 = sys.argv[2]
+x1 = int(x1)
+x2 = int(x2)
+print(f"Give me the first number:",x1)
+print(f"Give me the second number:",x2)
+
 print("Thank you!")
 print(f"{x1} + {x2} = {int(x1+x2)}")
 print(f"{x1} - {x2} = {int(x1-x2)}")
