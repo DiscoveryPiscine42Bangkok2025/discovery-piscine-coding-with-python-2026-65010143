c4388e9ba108bb914b9a7a0568a464d888197c3b
65010143 <65010143@kmitl.ac.th>
2026-02-12 01:09:18 +0700

    Update parameters.py

--- cell05/ex04/parameters.py
+++ cell05/ex04/parameters.py
@@ -1,4 +1,4 @@
-x = input()
-y= x.split(" ")
-y_new = list(filter(lambda i: i != "",y))
-print(f"Number of parameters: {len(y_new)}")
+import sys
+y = sys.argv[1:]
+
+print(f"Number of parameters: {len(y)}")
