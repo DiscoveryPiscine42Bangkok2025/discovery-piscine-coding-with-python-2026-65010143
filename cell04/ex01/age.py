058f282c90291cbc20f8f157814725579120786e
65010143 <65010143@kmitl.ac.th>
2026-02-12 00:53:05 +0700

    Update age.py

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -1,4 +1,7 @@
-x = input(f"Please tell me your age: ")
+import sys
+x = sys.argv[1]
 x = int(x)
+print(f"Please tell me your age:",x)
+
 for i in [10,20,30]:
     print(f"In {i} years, you'll be {x+i} years old")
